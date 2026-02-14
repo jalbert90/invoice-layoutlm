614776de96038d6d8d66fdf36495ae0b726b5551
Jacob Albert <jalbert90@gmail.com>
2026-02-14 10:23:46 -0500

    chore(tools): update labeling script to label all images

--- src/label.py
+++ src/label.py
@@ -1,25 +1,16 @@
 import json
 from pathlib import Path
 
-# Obtain path object for file
-# Open JSON file in rw mode
-# Read in as dict
-# Add a 'labels' key
-# Make every entry '0' except the 6th, which should be 'seller_name'
-# Write dict to JSON
-
 ocr_dir = Path('data/ocr')
 
-gen = ocr_dir.glob('*')
-test_json_path = next(gen)
+for json_path in ocr_dir.glob('*'):
+    print(f'Labeling {json_path}')
 
-print(test_json_path)
+    with open(json_path, 'r') as f:
+        data = json.load(f)
 
-with open(test_json_path, 'r') as f:
-    data = json.load(f)
+    data['labels'] = ['0' for token in data['tokens']]
+    data['labels'][5] = 'seller_name'
 
-data['labels'] = ['0' for token in data['tokens']]
-
-data['labels'][5] = 'seller_name'
-
-print(data)
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=2)
