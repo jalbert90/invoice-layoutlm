f3f5f7f3df05ba0d82bd98715dae4ca2b3bb19e5
Jacob Albert <jalbert90@gmail.com>
2026-02-15 13:57:06 -0500

    feat(layoutlm): extract data from ocr JSON file and update requirements with soft pins

--- src/layoutlm.py
+++ src/layoutlm.py
@@ -1,13 +1,32 @@
+import json
+from pathlib import Path
 from transformers import LayoutLMv3Processor, LayoutLMv3ForTokenClassification
+
+ocr_dir = Path('data/ocr')
+gen = ocr_dir.glob('*0062*')
+test_ocr_path = next(gen)
+
+with open(test_ocr_path, 'r') as f:
+    ocr_data = json.load(f)
+
+tokens = ocr_data['tokens']
+bboxes = ocr_data['bboxes']
+labels = ocr_data['labels']
+
+label2id = {label: i for i, label in enumerate(set(labels))}
+id2label = {i: label for label, i in label2id.items()}
 
 # Tokenize words (OCR "tokens"), convert tokens to ids, duplicate bounding boxes,
 # and preprocess images.
-processor = LayoutLMv3Processor.from_pretrained(
-    'microsoft/layoutlmv3-base',
-    apply_ocr=False
-)
+# processor = LayoutLMv3Processor.from_pretrained(
+#     'microsoft/layoutlmv3-base',
+#     apply_ocr=False
+# )
 
-model = LayoutLMv3ForTokenClassification.from_pretrained(
-    'microsoft/layoutlmv3-base',
-    # num_labels=?
-)
+# Load the base weigths.
+# model = LayoutLMv3ForTokenClassification.from_pretrained(
+#     'microsoft/layoutlmv3-base',
+#     num_labels=len(label2id),
+#     id2label=id2label,
+#     label2id=label2id
+# )
