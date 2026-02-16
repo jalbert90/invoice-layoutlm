b9500f615428cf01d2bb56fd3b61c57e51755223
Jacob Albert <jalbert90@gmail.com>
2026-02-16 13:09:42 -0500

    chore(label): update path variable name for clarity and consistency

--- src/label.py
+++ src/label.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 ocr_dir = Path('data/ocr')
 
-for json_path in ocr_dir.glob('*'):
-    print(f'Labeling {json_path}')
+for ocr_path in ocr_dir.glob('*'):
+    print(f'Labeling {ocr_path}')
 
-    with open(json_path, 'r') as f:
+    with open(ocr_path, 'r') as f:
         data = json.load(f)
 
     data['labels'] = ['0' for token in data['tokens']]
     data['labels'][5] = 'seller_name'
 
-    with open(json_path, 'w') as f:
+    with open(ocr_path, 'w') as f:
         json.dump(data, f, indent=2)
