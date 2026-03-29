906153a0dfd380da5f9024b5b2631754fe395414
Jacob Albert <jalbert90@gmail.com>
2026-03-29 14:25:28 -0400

    feat(inference, layoutlm_testing): update infer function to accept model directory and enhance output logging

--- src/inference.py
+++ src/inference.py
@@ -16,7 +16,7 @@ def softmax(logits):
 
     return [(math.e ** l) / s for l in logits]
 
-def infer(input_dir, ocr_save_dir, debug_dir):
+def infer(model_dir, input_dir, ocr_save_dir, debug_dir):
     ocr_docs = ocr_pipeline(input_dir, ocr_save_dir, debug_dir=debug_dir)
 
     image_paths = [doc["image_path"] for doc in ocr_docs]
@@ -26,12 +26,16 @@ def infer(input_dir, ocr_save_dir, debug_dir):
         apply_ocr=False
     )    
 
-    model_dir = Path('layoutlmv3-client/checkpoint-16')
-    model = LayoutLMv3ForTokenClassification.from_pretrained(str(model_dir))
+    model = LayoutLMv3ForTokenClassification.from_pretrained(model_dir)
 
     # Behaves like a list of dict[str, tensor]
     encoded_dataset = InvoiceDataset(ocr_docs, processor)
-    sample_num = 0
+    
+    print('\n\nSample Number | Sample Path\n')
+    for i in range(len(image_paths)):
+        print(f'{i}\t{image_paths[i]}')
+
+    sample_num = int(input('Enter sample number: '))
     sample = encoded_dataset[sample_num]
 
     model.eval()
@@ -39,9 +43,12 @@ def infer(input_dir, ocr_save_dir, debug_dir):
     with torch.no_grad():
         outputs = model(**sample)
 
+    model_dir = Path(model_dir)
     config_path = model_dir / f'config.json'
     with open(config_path) as f:
         config_data = json.load(f)
+
+    client_name_index = config_data['label2id']['client_name']
 
     print(f'\n\nImage Path:\n{image_paths[sample_num]}')
 
@@ -57,14 +64,22 @@ def infer(input_dir, ocr_save_dir, debug_dir):
 
     probabilities = torch.tensor(probabilities)
     print('\nProbabilities:\n')
+    print(probabilities.shape)
     print(probabilities)
 
-    index_0_label = config_data['id2label']['0']
-    print(f'\nProb token is {index_0_label} | Token\n')
+    probs = torch.softmax(outputs.logits, dim=-1)
+
+    print('\n\nBetter probs:\n')
+    print(probs.shape)
+    print(probs)
+
+    pred_ids = outputs.logits.argmax(dim=-1)
+
+    print(f'\nProb token is client_name | Token\n')
 
     count = 0
     for token in ocr_docs[sample_num]["tokens"]:
-        print(round(probabilities[count][0].item(), 2), '\t', token)
+        print(round(probabilities[count][client_name_index].item(), 2), '\t', token)
         count += 1
 
     client_names = {}
@@ -72,13 +87,15 @@ def infer(input_dir, ocr_save_dir, debug_dir):
     return client_names
 
 if __name__ == '__main__':
+    MODEL_DIR = 'models/batch1_1/checkpoint-95'
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', type=str, default='data/3_inference_pipeline/1_images/default')
     parser.add_argument('--ocr_save_dir', type=str, default='data/3_inference_pipeline/2_ocr/default')
     parser.add_argument('--debug_dir', type=str, default=None)
     args = parser.parse_args()
 
-    client_names = infer(args.input_dir, args.ocr_save_dir, args.debug_dir)
+    client_names = infer(MODEL_DIR, args.input_dir, args.ocr_save_dir, args.debug_dir)
 
     for invoice_name, client_name in client_names.items():
         print(f'{invoice_name}: \t {client_name}')
--- src/layoutlm_testing.py
+++ src/layoutlm_testing.py
@@ -6,6 +6,8 @@ from PIL import Image
 from torch.utils.data import Dataset
 from transformers import LayoutLMv3ForTokenClassification, LayoutLMv3Processor
 
+OCR_DIR = 'data/3_inference_pipeline/2_ocr/default'
+
 class InvoiceDataset(Dataset):
     def __init__(self, docs, processor, label2id):
         self.docs = docs
@@ -41,10 +43,16 @@ class InvoiceDataset(Dataset):
 
         return encoding
 
-ocr_dir = Path('data/2_training_pipeline/2_ocr/default')
+ocr_dir = Path(OCR_DIR)
 docs = []
 
-gen = ocr_dir.glob('*')
+gen = ocr_dir.glob('*0003*')
+
+# for ocr_path in islice(gen, 5):
+#     with open(ocr_path, 'r') as f:
+#         doc = json.load(f)
+#     docs.append(doc)
+
 test_ocr_path = next(gen)
 
 with open(test_ocr_path, 'r') as f:
@@ -58,8 +66,8 @@ for ocr_path in islice(ocr_dir.glob('*'), 5):
 
     docs.append(doc)
 
-label2id = {label: i for i, label in enumerate(set(docs[0]['labels']))}
-id2label = {i: label for label, i in label2id.items()}
+# label2id = {label: i for i, label in enumerate(set(docs[0]['labels']))}
+# id2label = {i: label for label, i in label2id.items()}
 
 # Tokenize words (OCR "tokens"), convert tokens to ids, duplicate bounding boxes,
 # and preprocess images.
@@ -69,31 +77,35 @@ processor = LayoutLMv3Processor.from_pretrained(
 )
 
 # Load the base weigths.
-model = LayoutLMv3ForTokenClassification.from_pretrained(
-    'microsoft/layoutlmv3-base',
-    num_labels=len(label2id),
-    id2label=id2label,
-    label2id=label2id
-)
+# model = LayoutLMv3ForTokenClassification.from_pretrained(
+#     'microsoft/layoutlmv3-base',
+#     num_labels=len(label2id),
+#     id2label=id2label,
+#     label2id=label2id
+# )
 
 encoding = processor(
             images=test_image,
             text=test_doc['tokens'],
             boxes=test_doc['bboxes'],
-            word_labels=[label2id[label] for label in test_doc['labels']],
+            # word_labels=[label2id[label] for label in test_doc['labels']],
             padding='max_length',
             truncation=True,
-            max_length=128,
+            # max_length=128,
             return_tensors='pt'
         )
 
-data = InvoiceDataset(docs, processor, label2id)
+print(encoding['input_ids'].shape)
+print(encoding['input_ids'])
+print(encoding)
+
+# data = InvoiceDataset(docs, processor, label2id)
 
 # print(encoding)
 # print(data[0])
 
-for k, v in encoding.items():
-    print(k, v.shape)
+# for k, v in encoding.items():
+#     print(k, v.shape)
 
-for k, v in data[0].items():
-    print(k, v.shape)
+# for k, v in data[0].items():
+#     print(k, v.shape)
